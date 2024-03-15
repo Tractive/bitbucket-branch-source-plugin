3e6266def449035e3610d20f99731b1f7fb52827
Thomas Salzinger <thomas.salzinger@tractive.com>
2024-03-15 12:50:09 +0100

    Replace reflective creation of HookProcessors with a Supplier
    Using a supplier removes the need to deal with any reflective specific exceptions.

--- src/main/java/com/cloudbees/jenkins/plugins/bitbucket/hooks/HookEventType.java
+++ src/main/java/com/cloudbees/jenkins/plugins/bitbucket/hooks/HookEventType.java
@@ -25,6 +25,7 @@ package com.cloudbees.jenkins.plugins.bitbucket.hooks;
 
 import edu.umd.cs.findbugs.annotations.CheckForNull;
 import edu.umd.cs.findbugs.annotations.NonNull;
+import java.util.function.Supplier;
 
 /**
  * Bitbucket hooks types managed by this plugin.
@@ -34,101 +35,101 @@ public enum HookEventType {
     /**
      * See <a href="https://confluence.atlassian.com/bitbucket/event-payloads-740262817.html#EventPayloads-Push">EventPayloads-Push</a>
      */
-    PUSH("repo:push", PushHookProcessor.class),
+    PUSH("repo:push", PushHookProcessor::new),
 
     /**
      * See <a href="https://confluence.atlassian.com/bitbucket/event-payloads-740262817.html#EventPayloads-Created.1">EventPayloads-Created</a>
      */
-    PULL_REQUEST_CREATED("pullrequest:created", PullRequestHookProcessor.class),
+    PULL_REQUEST_CREATED("pullrequest:created", PullRequestHookProcessor::new),
 
     /**
      * See <a href="https://confluence.atlassian.com/bitbucket/event-payloads-740262817.html#EventPayloads-Updated.1">EventPayloads-Updated</a>
      */
-    PULL_REQUEST_UPDATED("pullrequest:updated", PullRequestHookProcessor.class),
+    PULL_REQUEST_UPDATED("pullrequest:updated", PullRequestHookProcessor::new),
 
     /**
      * See <a href="https://confluence.atlassian.com/bitbucket/event-payloads-740262817.html#EventPayloads-Merged">EventPayloads-Merged</a>
      */
-    PULL_REQUEST_MERGED("pullrequest:fulfilled", PullRequestHookProcessor.class),
+    PULL_REQUEST_MERGED("pullrequest:fulfilled", PullRequestHookProcessor::new),
 
     /**
      * See <a href="https://confluence.atlassian.com/bitbucket/event-payloads-740262817.html#EventPayloads-Declined">EventPayloads-Declined</a>
      */
-    PULL_REQUEST_DECLINED("pullrequest:rejected", PullRequestHookProcessor.class),
+    PULL_REQUEST_DECLINED("pullrequest:rejected", PullRequestHookProcessor::new),
 
     /**
      * See <a href="https://confluence.atlassian.com/bitbucket/event-payloads-740262817.html#EventPayloads-Approved">EventPayloads-Approved</a>
      */
-    PULL_REQUEST_APPROVED("pullrequest:approved", PullRequestHookProcessor.class),
+    PULL_REQUEST_APPROVED("pullrequest:approved", PullRequestHookProcessor::new),
 
     /**
      * @see <a href="https://confluence.atlassian.com/bitbucketserver054/event-payload-939508609.html#Eventpayload-Push">Eventpayload-Push</a>
      * @since Bitbucket Server 5.4
      */
-    SERVER_REFS_CHANGED("repo:refs_changed", NativeServerPushHookProcessor.class),
+    SERVER_REFS_CHANGED("repo:refs_changed", NativeServerPushHookProcessor::new),
 
     /**
      * @see <a href="https://confluence.atlassian.com/bitbucketserver054/event-payload-939508609.html#Eventpayload-Opened">Eventpayload-Opened</a>
      * @since Bitbucket Server 5.4
      */
-    SERVER_PULL_REQUEST_OPENED("pr:opened", NativeServerPullRequestHookProcessor.class),
+    SERVER_PULL_REQUEST_OPENED("pr:opened", NativeServerPullRequestHookProcessor::new),
 
     /**
      * @see <a href="https://confluence.atlassian.com/bitbucketserver054/event-payload-939508609.html#Eventpayload-Merged">Eventpayload-Merged</a>
      * @since Bitbucket Server 5.4
      */
-    SERVER_PULL_REQUEST_MERGED("pr:merged", NativeServerPullRequestHookProcessor.class),
+    SERVER_PULL_REQUEST_MERGED("pr:merged", NativeServerPullRequestHookProcessor::new),
 
     /**
      * See <a href="https://confluence.atlassian.com/bitbucketserver054/event-payload-939508609.html#Eventpayload-Declined">Eventpayload-Declined</a>
      * @since Bitbucket Server 5.4
      */
-    SERVER_PULL_REQUEST_DECLINED("pr:declined", NativeServerPullRequestHookProcessor.class),
+    SERVER_PULL_REQUEST_DECLINED("pr:declined", NativeServerPullRequestHookProcessor::new),
 
     /**
      * See <a href="https://confluence.atlassian.com/bitbucketserver054/event-payload-939508609.html#Eventpayload-Deleted">Eventpayload-Deleted</a>
      *
      * @since Bitbucket Server 5.4
      */
-    SERVER_PULL_REQUEST_DELETED("pr:deleted", NativeServerPullRequestHookProcessor.class),
+    SERVER_PULL_REQUEST_DELETED("pr:deleted", NativeServerPullRequestHookProcessor::new),
 
     /**
      * See <a href="https://confluence.atlassian.com/bitbucketserver054/event-payload-939508609.html#Eventpayload-Approved">Eventpayload-Approved</a>
      *
      * @since Bitbucket Server 5.4
      */
-    SERVER_PULL_REQUEST_APPROVED("pr:reviewer:approved", NativeServerPullRequestHookProcessor.class),
+    SERVER_PULL_REQUEST_APPROVED("pr:reviewer:approved", NativeServerPullRequestHookProcessor::new),
 
     /**
      * @see <a href="https://confluence.atlassian.com/bitbucketserver0510/event-payload-951390742.html#Eventpayload-Modified.1">Eventpayload: Pull Request - Modified</a>
      * @since Bitbucket Server 5.10
      */
-    SERVER_PULL_REQUEST_MODIFIED("pr:modified", NativeServerPullRequestHookProcessor.class),
+    SERVER_PULL_REQUEST_MODIFIED("pr:modified", NativeServerPullRequestHookProcessor::new),
 
     /**
      * @see <a href="https://confluence.atlassian.com/bitbucketserver0510/event-payload-951390742.html#Eventpayload-ReviewersUpdated">Eventpayload: Pull Request - Reviewers Updated</a>
      * @since Bitbucket Server 5.10
      */
-    SERVER_PULL_REQUEST_REVIEWER_UPDATED("pr:reviewer:updated", NativeServerPullRequestHookProcessor.class),
+    SERVER_PULL_REQUEST_REVIEWER_UPDATED("pr:reviewer:updated", NativeServerPullRequestHookProcessor::new),
 
     /**
      * @see <a href="https://confluence.atlassian.com/bitbucketserver070/event-payload-996644369.html#Eventpayload-Sourcebranchupdated">Eventpayload-Sourcebranchupdated</a>
      * @since Bitbucket Server 7.0
      */
-    SERVER_PULL_REQUEST_FROM_REF_UPDATED("pr:from_ref_updated", NativeServerPullRequestHookProcessor.class),
+    SERVER_PULL_REQUEST_FROM_REF_UPDATED("pr:from_ref_updated", NativeServerPullRequestHookProcessor::new),
 
     /**
      * Sent when hitting the {@literal "Test connection"} button in Bitbucket Server. Apparently undocumented.
      */
-    SERVER_PING("diagnostics:ping", PingHookProcessor.class);
+    SERVER_PING("diagnostics:ping", PingHookProcessor::new);
 
 
     private final String key;
-    private final Class<?> clazz;
+    private final Supplier<? extends HookProcessor> hookProcessorFactory;
 
-    <P extends HookProcessor> HookEventType(@NonNull String key, Class<P> clazz) {
+    <P extends HookProcessor> HookEventType(@NonNull String key, Supplier<P> hookProcessorFactory) {
         this.key = key;
-        this.clazz = clazz;
+        this.hookProcessorFactory = hookProcessorFactory;
     }
 
     @CheckForNull
@@ -142,11 +143,7 @@ public enum HookEventType {
     }
 
     public HookProcessor getProcessor() {
-        try {
-            return (HookProcessor) clazz.newInstance();
-        } catch (InstantiationException | IllegalAccessException e) {
-            throw new AssertionError("Can not instantiate hook payload processor: " + e.getMessage());
-        }
+        return hookProcessorFactory.get();
     }
 
     public String getKey() {
